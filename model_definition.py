d785e66d5e1f5d709fe90973a067abbe6fca847f
karasyuk <karasyuk@promsoft.ru>
2022-05-06 11:05:00 +0700

    25 minutes 30 seconds

--- model_definition.py
+++ model_definition.py
@@ -48,7 +48,7 @@ class ThemeTransformer(pl.LightningModule):
         return train_loader
 
     def configure_optimizers(self):
-        t = torch.optim.Adam(self.transformer.parameters(), lr=self.args.lr // 2)
+        t = torch.optim.Adam(self.transformer.parameters(), lr=self.args.lr)
         return [t], \
                [torch.optim.lr_scheduler.CosineAnnealingLR(t, T_max=self.args.max_step, eta_min=self.args.lr_min)]
 
